5ecc64ca8d2384e09cf5763759584d579ce6d289
Wonjoon Lee <dnjswns775@gmail.com>
2023-06-28 23:40:29 +0900

    init commit

---
+++ src/main/java/team34/tarot/TarotApplication.java
@@ -0,0 +1,13 @@
+package team34.tarot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team34/tarot/TarotApplicationTests.java
@@ -0,0 +1,13 @@
+package team34.tarot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
