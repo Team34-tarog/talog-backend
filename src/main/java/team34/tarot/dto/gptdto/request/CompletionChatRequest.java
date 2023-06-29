c9b6ffd388d88de1db51ea40f3151c539f3b5428
anso33 <hyeon131812@gmail.com>
2023-06-29 23:46:49 +0900

    feat: Implement dto & service for gpt-request

--- src/main/java/team34/tarot/dto/gptdto/request/CompletionChatRequest.java
+++ src/main/java/team34/tarot/dto/gptdto/request/CompletionChatRequest.java
@@ -1,23 +1,27 @@
-package team34.tarot.dto.request;
+package team34.tarot.dto.gptdto.request;
 
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompletionChatRequest {
 
 	private String model;
 	private String role;
 	private String message;
 	private Integer maxTokens;
-
-	public static ChatCompletionRequest of(String model, String role, String message, Integer maxTokens) {
+	
+	public static ChatCompletionRequest of(CompletionChatRequest request) {
 		return ChatCompletionRequest.builder()
-						.model(model)
-						.messages(convertChatMessage(role, message))
-						.maxTokens(maxTokens)
+						.model(request.getModel())
+						.messages(convertChatMessage(request.getRole(), request.getMessage()))
+						.maxTokens(request.getMaxTokens())
 						.build();
 	}
 
--- src/main/java/team34/tarot/service/GPTService.java
+++ src/main/java/team34/tarot/service/GPTService.java
@@ -1,9 +1,14 @@
 package team34.tarot.service;
 
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
 import com.theokanning.openai.service.OpenAiService;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import team34.tarot.dto.request.CompletionChatRequest;
+import team34.tarot.dto.gptdto.request.CompletionChatRequest;
+import team34.tarot.dto.gptdto.response.CompletionChatResponse;
 
 @RequiredArgsConstructor
 @Service
@@ -11,7 +16,17 @@ public class GPTService {
 
 	private final OpenAiService openAiService;
 
-	public CompletionChatResponse gptCompletionChat(CompletionChatRequest request) {
+	@Transactional
+	public List<String> gptCompletionChat(CompletionChatRequest request) {
+		ChatCompletionResult chatCompletion = openAiService.createChatCompletion(
+						CompletionChatRequest.of(request));
 
+		CompletionChatResponse response = CompletionChatResponse.of(chatCompletion);
+
+		List<String> messages = response.getMessages().stream()
+						.map(CompletionChatResponse.Message::getMessage)
+						.collect(Collectors.toList());
+
+		return messages;
 	}
 }
