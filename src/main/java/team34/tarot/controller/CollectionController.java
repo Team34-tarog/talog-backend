4a8032f20ebb5fd3f49c0a6dd33b7a40712b6e6c
anso33 <hyeon131812@gmail.com>
2023-06-29 22:32:00 +0900

    feat: Apply security about userId

--- src/main/java/team34/tarot/controller/CollectionController.java
+++ src/main/java/team34/tarot/controller/CollectionController.java
@@ -3,12 +3,10 @@ package team34.tarot.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team34.tarot.auth.CustomUserDetails;
 import team34.tarot.dto.response.CollectionResponse;
-import team34.tarot.entity.User;
 import team34.tarot.service.CollectionService;
 
 @RequiredArgsConstructor
@@ -18,12 +16,12 @@ public class CollectionController {
 
 	private final CollectionService collectionService;
 
-	@GetMapping("/cards")
+	@GetMapping("")
 	public CollectionResponse getCollection() {
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		CustomUserDetails userDetails = (CustomUserDetails) principal;
-		Long username = (userDetails).getId();
-		return collectionService.getCollection(username);
+		Long userId = (userDetails).getId();
+		return collectionService.getCollection(userId);
 	}
 
 }
--- src/main/java/team34/tarot/controller/QnaController.java
+++ src/main/java/team34/tarot/controller/QnaController.java
@@ -3,13 +3,14 @@ package team34.tarot.controller;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import team34.tarot.auth.CustomUserDetails;
 import team34.tarot.dto.request.PostQuestionRequest;
 import team34.tarot.dto.response.QnaResponse;
 import team34.tarot.service.QnaService;
@@ -21,15 +22,20 @@ public class QnaController {
 
 	private final QnaService qnaService;
 
-	@PostMapping("question/{userId}")
-	public void postQuestion(@PathVariable(name = "userId") Long userId, @RequestBody PostQuestionRequest request) {
+	@PostMapping("question/")
+	public void postQuestion(@RequestBody PostQuestionRequest request) {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		CustomUserDetails userDetails = (CustomUserDetails) principal;
+		Long userId = (userDetails).getId();
 		qnaService.postQuestion(userId, request);
 	}
 
-	@GetMapping("/{userId}")
+	@GetMapping("")
 	public QnaResponse getQna(
-					@PathVariable(name = "userId") Long userId,
 					@RequestParam(name = "date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		CustomUserDetails userDetails = (CustomUserDetails) principal;
+		Long userId = (userDetails).getId();
 		return qnaService.getQna(userId, date);
 	}
 
