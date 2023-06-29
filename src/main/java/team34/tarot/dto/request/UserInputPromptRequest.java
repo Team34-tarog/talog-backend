25392b67ea1d32016818e6b6e8ac5db239b2617e
Wonjoon Lee <dnjswns775@gmail.com>
2023-06-30 00:15:59 +0900

    Add : UserInputPrompt

---
+++ src/main/java/team34/tarot/dto/request/UserInputPromptRequest.java
@@ -0,0 +1,11 @@
+package team34.tarot.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class UserInputPromptRequest {
+    String username;
+    String gender;
+    int age;
+}
---
+++ src/main/java/team34/tarot/service/PromptService.java
@@ -0,0 +1,19 @@
+package team34.tarot.service;
+
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
+import org.springframework.stereotype.Service;
+import team34.tarot.dto.request.UserInputPromptRequest;
+
+@Service
+public class PromptService {
+
+
+    //사람 입력 프롬프트 함수 (스트링 반환)
+    public String systemChatUserInput(UserInputPromptRequest request) {
+        String s = "+, gender: " + request.getGender() + ", and age range: " + request.getAge() + ". As a tarot reader, you need to understand the personality of your tarot customer and answer them to the best of your ability.Finally, all questions are written in PlainTEXT only. Don't use any markdown syntax.";
+        return s;
+    }
+
+    public String systemDiaryInput(Diary)
+
+}
